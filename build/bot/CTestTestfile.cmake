04f8ba5a67e2b2a51c575d4c3cace540876fe291
dariustanrd <tan.rongde.darius@gmail.com>
2019-03-07 17:56:35 +0800

    Removed files

--- build/bot/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/darius/ROS_Workspaces/proj1_ws/src/bot
-# Build directory: /home/darius/ROS_Workspaces/proj1_ws/build/bot
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
